e752f9fa8916b095d0daabfbea6ba96d55472a79
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2024-11-16 20:10:07 +0600

    backend setup completed

---
+++ packages/server/src/index.ts
@@ -0,0 +1,16 @@
+import  express from "express";
+
+
+const app = express() ;
+
+const port = process.env.PORT || 4000 ;
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Congratulations! you can see the data from backend."
+    })
+});
+
+app.listen(port, () => {
+    console.log("Server is working in the port", port)
+})
