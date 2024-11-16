8e08c563be4d8ff2b68107950ff8fb19de5cf470
Najmul Huda Chowdhury Chayan <najmulhc@outlook.com>
2024-11-16 20:44:39 +0600

    data fetching part changed

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,13 +1,10 @@
-const getData = async () => {
+ 
+const HomePage = async () => {
   const res = await fetch(process.env.NEXT_SERVER_URL as string);
   const data = await res.json();
-  return data.message;
-};
-const HomePage = async () => {
-  const message = await getData();
   return (
     <main>
-      <h1>{message ? message : "not any data from server"} </h1>
+      <h1>{data?.message} </h1>
     </main>
   );
 };
